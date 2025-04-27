5ff137a71b5791ec4ae698b787f9d4fcce478274
王群銘 <w6150038@hotmail.com>
2025-04-27 10:26:04 +0800

    update_1140427_1

---
+++ script.js
@@ -0,0 +1,14 @@
+// 載入 header.html 和 footer.html
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('header.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('header').innerHTML = data;
+      });
+  
+    fetch('footer.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('footer').innerHTML = data;
+      });
+});
